a43d00a0f602dffe8d41aadf5874d82df21b64be
skj <shubhanshujain2233@gmail.com>
2024-07-08 22:28:55 +0530

    routerChange

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { LOGO_URL } from "../utils/contant";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Header = () => {
+  //if no dependency array then use effect is called after every render
+  // if dependency array is empty = [] => useEffect is called on initial render
+  // if dependency array is [btnNameReact] => called everytime btnNameReact when is updated
+  useEffect(() => {}, []);
   const [LoginButton, setLoginButton] = useState("Login");
   return (
     <div className="header">
