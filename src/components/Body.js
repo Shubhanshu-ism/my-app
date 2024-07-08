9428ed2d3d15be6c1049a11e42cd769e62916c88
skj <shubhanshujain2233@gmail.com>
2024-07-08 17:56:53 +0530

    code=

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,68 +1,89 @@
 import ResDataCard from "./ResDataCart";
-import {useState, useEffect} from "react";
-import resList from "../utils/mock";
+import { useState, useEffect } from "react";
+
 import Shimmer from "./shimmer";
-const Body = ()=>{
-  const [ListOfRestaurant,setListOfRestaurant] = useState([...resList]);
-  
-  const [SeachText,setSearchText] = useState("");
-  useEffect(()=> {fetchData()},[]);
-    
-    const fetchData = async () => {
-      const data = await fetch(
-          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.8191134&lng=86.4359561&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      );
-      const json = await data.json();
-      console.log(json);
-      const resturants = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-      
-      if(ListOfRestaurant.length === 0){
-        return <Shimmer/>;
-      }
-      if(resturants.length) setListOfRestaurant( resturants);
-      else setListOfRestaurant(resList);
+const Body = () => {
+  const [ListOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [SeachText, setSearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.8191134&lng=86.4359561&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    const resturants =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || [];
+
+    if (resturants.length) {
+      setListOfRestaurant(resturants);
+      setFilteredRestaurant(resturants);
     }
-    
-    
-    return ( 
-      <div className="body">
-        <input type="text" value={SeachText} onChange={(e) =>{setSearchText(e.target.value)}}></input>
-        <button className="search-button" onClick={()=>{
-          const searchFilter = ListOfRestaurant.filter((x) => {
-            const f1= x.info.name.toLowerCase().includes(SeachText.toLowerCase());
-           
-           
-            return f1 ;
-          } );
+  };
 
-          setListOfRestaurant(searchFilter);
-        }}
-        >
-          Search
+  return ListOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="fliters">
+        {/* <div className="search"> */}
+          <input
+            type="text"
+            value={SeachText}
+            placeholder="Search a restaurant..."
+            className="searchBox"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-button"
+            onClick={() => {
+              const filteredRestaurant = ListOfRestaurant.filter((x) =>
+                x.info.name.toLowerCase().includes(SeachText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
           </button>
-        <button className="top-rated-button" onClick={ () => {
-          const FilterList = ListOfRestaurant.filter((x) => x.info.avgRating >4);
-          setListOfRestaurant(FilterList);
-          }
-        }
+        {/* </div> */}
+        <button
+          className="top-rated-button"
+          onClick={() => {
+            const filterList = ListOfRestaurant.filter(
+              (x) => x.info.avgRating > 4
+            );
+            setFilteredRestaurant(filterList);
+          }}
         >
           Top Rated
         </button>
-        
-        <button className="reset-button" onClick={() =>{
-          fetchData();
-          // setListOfRestaurant(ListOfRestaurant);
-        }}>Reset</button>
-        {/* <div className="search">Search</div> */}
-        <div className="res-container">
-        {
-          ListOfRestaurant.map((x) => <ResDataCard key={x?.info?.id} resData={x}/>)
-        }
-     
-        </div>
-          
+
+        <button
+          className="reset-button"
+          onClick={() => {
+            
+            setFilteredRestaurant(ListOfRestaurant);
+          }}
+        >
+          Reset
+        </button>
       </div>
-    )
-  }
+
+      {/* Resutant containner */}
+      <div className="res-container">
+        {filteredRestaurant.map((x) => (
+          <ResDataCard key={x?.info?.id} resData={x} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Body;
--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,23 +1,26 @@
-const Shimmer =  () =>{
-    return ( 
-        <div>
-            <button className="top-rated-button"> top Rated</button>
-            <button className="reset-button">Reset</button>
-            <div className="shimmer-container">
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                <div className="shimmer-card"></div>
-                
-            </div>
-        </div>
-    );
+const Shimmer = () => {
+  return (
+    <div>
+      <input type="text"></input>
+      <button className="search-button">Search</button>
+      <button className="top-rated-button"> Top Rated</button>
+      <button className="reset-button">Reset</button>
+      <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </div>
+  );
 };
 export default Shimmer;
-<button className="top-rated-button"> top Rated</button>
+{
+  /* <button className="top-rated-button"> top Rated</button> */
+}
